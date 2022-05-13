22be4730ee2d8bb0b95266d8dc2b12d682288c67
Remy Lagrois <rlagrois@oreilly.com>
2022-05-12 21:24:16 -0400

    ch1 ex2 stuff

--- Chapter01/ch01_ex2.py
+++ Chapter01/ch01_ex2.py
@@ -9,6 +9,7 @@ http://www.cs.kent.ac.uk/people/staff/dat/miranda/whyfp90.pdf -> Why Functional 
 
 Translated from Miranda to Python.
 """
+import timeit
 from typing import Callable, Iterator
 
 # next_ = lambda n, x: (x+n/x)/2
@@ -38,11 +39,24 @@ def next_(n: float, x: float) -> float:
     return (x+n/x)/2
 
 def repeat(f: Callable[[float], float], a: float) -> Iterator[float]:
-    """yields a, f(a), f(f(a)), etc."""
+    """
+    yields a, f(a), f(f(a)), etc.
+    function is to generate (potentially) infinite sequence of numbers
+    will be implemented where f is next_, therefore the sequence produced
+    is iterations of the approxamation algorithm
+    """
     yield a
-    yield from repeat(f, f(a))
+    yield from repeat(f, f(a)) # equivalent to for x in iter: yield x
 
 def within(eps: float, iterable: Iterator[float]) -> Iterator[float]:
+    """
+    Checks a given sequence to find a pair of numbers within that
+    are sufficiently close (ie < eps)
+
+    Inner function runs through the iterable until a number meets that condition
+
+    Outer function initializes
+    """
     def head_tail(eps: float, a: float, iterable: Iterator[float]):
         b = next(iterable)
         if abs(a-b) <= eps:
@@ -52,7 +66,7 @@ def within(eps: float, iterable: Iterator[float]) -> Iterator[float]:
     return head_tail(eps, next(iterable), iterable)
 
 def sqrt(a0: float, eps: float, n: float):
-    return within(eps, repeat(lambda x: next_(n, x), a0))
+    return within(eps, repeat(lambda x: next_(n, x), a0)) # n stays the same, only x is different
 
 def test():
     """
@@ -77,3 +91,6 @@ def test():
 
 if __name__ == "__main__":
     test()
+    print(sqrt(1.0, .0001, 3), f' - actual:  {3**0.5}')
+    print(sqrt(2.0, .0001, 8), f' - actual:  {8**0.5}')
+    print(sqrt(3.0, .0001, 15), f' - actual:  {15**0.5}')
