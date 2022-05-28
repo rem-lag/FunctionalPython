a53cd6d8f712c9e0e168c36212a8643a34ea12d7
Remy Lagrois <rlagrois@oreilly.com>
2022-05-28 16:29:47 -0400

    fix chunker

--- personal/chunker.py
+++ personal/chunker.py
@@ -1,14 +1,21 @@
 
 
 def make_chunks(item, batches):
+    """
+    Generator function
+    Takes list (or other slicable) and returns it
+    split into desired number of batches
+    Batches will be of equal size, except last
+    Last will include remainder of list items
+    """
     item_len = len(item)
     base_size = item_len//batches
     add_final = item_len%batches
     def chunker(chunk, start, stop, remain):
-        yield chunk[start:stop]
-        if remain == 0:
+        if remain == 1:
             yield chunk[start:stop+add_final]
             return
+        yield chunk[start:stop]
         yield from chunker(chunk, start+base_size, stop+base_size, remain-1)
     
     return chunker(item, 0, base_size, batches)
@@ -22,9 +29,9 @@ def main():
     batch_two = make_chunks(list_two, 4)
     batch_three = make_chunks(list_three, 8)
 
-    print(f'Batch One:\n\t{batch_one}')
-    print(f'Batch Two:\n\t{batch_two}')
-    print(f'Batch Three:\n\t{batch_three}')
+    print(f'Batch One:\n\t{list(batch_one)}')
+    print(f'Batch Two:\n\t{list(batch_two)}')
+    print(f'Batch Three:\n\t{list(batch_three)}')
 
 
 if __name__ == '__main__':
